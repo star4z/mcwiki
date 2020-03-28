6124392cf487a37393c2cdd9253fa99f13c3159d
Benjamin Phillips <bdphillips013@gmail.com>
2020-03-28 01:24:00 -0400

    Updated with blocks and BeautifulSoup

--- item_sorter.py
+++ item_sorter.py
@@ -1,8 +1,11 @@
 import csv
+import re
 from html.parser import HTMLParser
 from pprint import pprint
+import urllib.request
 
 import requests
+from bs4 import BeautifulSoup
 
 phrase = r'class="sprite item-sprite"'
 href = r'href='
@@ -10,135 +13,113 @@ title = r'title='
 
 
 def get_page(page) -> str:
-    S = requests.Session()
-
-    URL = "https://minecraft.gamepedia.com/api.php"
-
-    PARAMS = {
-        "action": "parse",
-        "page": page,
-        "format": "json"
-    }
-
-    R = S.get(url=URL, params=PARAMS)
-    DATA = R.json()
-
-    body = DATA["parse"]["text"]["*"]
-
-    if 'redirectMsg' in body:
-        href_index = body.index(href)
-        next_href = find_quote(body, href_index)
-
-        return get_page(next_href[1:])
-
+    fp = urllib.request.urlopen('https://minecraft.gamepedia.com' + page)
+    m_bytes = fp.read()
+    body = m_bytes.decode('utf8')
+    fp.close()
     return body
 
 
-def find_element(body, element, start_index):
-    opening_tag = f'<{element}>'
-    closing_tag = f'</{element}>'
-
-    opening_index = body.index(opening_tag, start_index)
-    closing_index = body.index(closing_tag, opening_index + len(opening_tag))
-
-    return body[opening_index + len(opening_tag): closing_index]
-
-
-def find_quote(span: str, start):
-    first_index = span.index('"', start)
-    second_index = span.index('"', first_index + 1)
-    return span[first_index + 1: second_index]
-
-
-class MyHTMLParser(HTMLParser):
-    def handle_charref(self, name):
-        if name.startswith('x'):
-            c = chr(int(name[1:], 16))
-        else:
-            c = chr(int(name))
-        print("Num ent  :", c)
-
-
 class Item:
-    def __init__(self, title, link, stackable=None, stacks=None, renewable=None):
+    def __init__(self, title, stackable=None, stacks=None, renewable=None):
         self.title = title
-        self.link = link
-        print(self.title + ':' + self.link)
         self.stackable = stackable
-        try:
-            self.stacks = int(stacks)
-        except ValueError:
-            self.stacks = None
+        self.stacks = stacks
         self.renewable = renewable
-        if self.stackable is None or self.renewable is None:
-            try:
-                self.page = get_page(self.link[1:].replace('%27', "'"))
-                stackable_index = self.page.index('Stackable')
-                stackable_str = find_element(self.page, 'p', stackable_index)[:-1]
-                self.stackable = 'Yes' in stackable_str
-                if self.stackable:
-                    p1 = stackable_str.index('(')
-                    p2 = stackable_str.index(')')
-                    self.stacks = int(stackable_str[p1 + 1: p2])
-                else:
-                    self.stacks = None
-                self.stackable = None
-                self.stacks = None
-                renewable_index = self.page.index('Renewable')
-                renewable_str = find_element(self.page, 'p', renewable_index)[:-1]
-                self.renewable = 'Yes' in renewable_str
-            except (KeyError, ValueError) as e:
-                print(e)
 
     def __repr__(self):
         return self.title
 
 
-def get_results_from_wiki():
-    global results
-    body = get_page('Item')
-    begin = 0
+def get_items():
+    body = get_page('/Item')
+    soup = BeautifulSoup(body, 'html.parser')
     results = []
-    while phrase in body[begin:]:
-        next_index = body.index(phrase, begin)
-        href_index = body.index(href, next_index)
-        title_index = body.index(title, next_index)
+    for span in soup.find_all('span', 'item-sprite'):
+        a = span.parent.a
+        print(a.get_text())
+        results.append(get_item(a.get_text(), a['href']))
 
-        next_href = find_quote(body, href_index)
-        next_title = find_quote(body, title_index)
-        results.append(Item(next_title, next_href))
-        begin = next_index + 1
+    return results
+
+
+def get_item(title, link):
+    page = get_page(link)
+    soup = BeautifulSoup(page, 'html.parser')
+
+    try:
+        stackable_str = soup.table.find('th', text=re.compile('Stackable')).parent.p.get_text()
+        stackable = boolean_with_versioning(stackable_str)
+        if stackable:
+            p1 = stackable_str.index('(')
+            p2 = stackable_str.index(')')
+            stacks = int(stackable_str[p1 + 1: p2])
+        else:
+            stacks = None
+    except AttributeError:
+        stackable = None
+        stacks = None
+
+    try:
+        renewable_text = soup.table.find('a', text='Renewable').parent.parent.p.get_text()
+        renewable = boolean_with_versioning(renewable_text)
+    except (TypeError, AttributeError):
+        renewable = None
+
+    return Item(title, stackable, stacks, renewable)
+
+
+def boolean_with_versioning(value_str):
+    if 'JE' in value_str:
+        # I'm grossly assuming here that the JE version will always be the first value in the second half
+        return 'Yes' in value_str[len(value_str) // 2:]
+    else:
+        return 'Yes' in value_str
+
+
+def get_blocks():
+    html = get_page('/Blocks')
+    soup = BeautifulSoup(html, 'html.parser')
+    cells = soup.table.find_all('td')
+
+    results = []
+    for cell in cells:
+        a = cell.find_all('a')[-1]
+        block_title = a.string
+        block_link = a['href']
+
+        print(block_title)
+
+        results.append(get_item(block_title, block_link))
 
     return results
 
 
 def get_results_from_file():
-    results = []
-
     with open('items.csv', newline='') as csv_file:
         reader = csv.reader(csv_file)
+        # Skip header line
         next(reader)
-        for result in reader:
-            results.append(Item(result[0], result[1], result[2], result[3], result[4]))
-
-    return results
+        return [Item(*result) for result in reader]
 
 
-def save_to_file():
-    table = [[item.title, item.link, item.stackable, item.stacks, item.renewable] for item in results]
+def save_to_file(items):
+    table = [[item.title, item.stackable, item.stacks, item.renewable] for item in items]
     pprint(table)
     with open('items.csv', 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(['title', 'link', 'stackable', 'stacks', 'renewable'])
+        # Header for convenience
+        writer.writerow(['title', 'stackable', 'stacks', 'renewable'])
         for row in table:
             writer.writerow(row)
 
 
 if __name__ == '__main__':
-    results = get_results_from_file()
-    # if not results:
-    #     results = get_results_from_wiki()
-    #     save_to_file()
+    try:
+        results = get_results_from_file()
+    except FileNotFoundError:
+        results = get_blocks() + get_items()
+        save_to_file(results)
 
     print(f'Total items: {len(results)}')
     print('16 stacking items:')
